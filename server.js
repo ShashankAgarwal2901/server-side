0c6a972dd3ffb6bb722e65485fcb0f77afc09957
ShashankAgarwal2901 <shashank.agarwal29197@gmail.com>
2016-10-08 06:29:25 +0530

    TEST

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-This is a test git uploadabcdawdmaodwm
+var express = require("express");
+
+var app = express();
+
+
+app.use(function(rq,re){
+	re.writeHead(200 , {'Content-Type' : 'text/html'});
+	re.end('<h1>Hello</h1>');
+});
+app.listen(3000,'localhost');
+console.log('Server is Running');
